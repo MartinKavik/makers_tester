2e760da40368721f48c58669fbf58e00fc093b7d
Martin Kavík <martin@kavik.cz>
2022-05-31 15:56:48 +0200

    countdown + Makefile.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Tester started");
+
+    let seconds = env::args()
+        .next_back()
+        .unwrap_or_default()
+        .parse()
+        .unwrap_or_default();
+
+    for remaining_seconds in (1..=seconds).rev() {
+        println!("Seconds to finish: {remaining_seconds}");
+        thread::sleep(Duration::from_secs(1));
+    }
+
+    println!("Tester finished");
 }
