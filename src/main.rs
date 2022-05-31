aeb56df0144b42b3459aacfdaecc27afada5d4e9
Martin Kavík <martin@kavik.cz>
2022-05-31 16:03:16 +0200

    ignore ctrl+c signal

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::{env, thread, time::Duration};
 
 fn main() {
-    println!("Tester started");
+    println!("Tester started.");
+
+    ctrlc::set_handler(|| println!("Tester ignores Ctrl+C signal."))
+        .expect("Failed to set Ctrl+C handler in Tester.");
 
     let seconds = env::args()
         .next_back()
@@ -10,9 +13,9 @@ fn main() {
         .unwrap_or_default();
 
     for remaining_seconds in (1..=seconds).rev() {
-        println!("Seconds to finish: {remaining_seconds}");
+        println!("Seconds to finish: {remaining_seconds}.");
         thread::sleep(Duration::from_secs(1));
     }
 
-    println!("Tester finished");
+    println!("Tester finished.");
 }
